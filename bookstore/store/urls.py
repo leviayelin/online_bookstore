92ac31163c512f8dee970d7d6544237a6829f421
Tomer <ttomerrtasma@gmail.com>
2025-01-18 20:37:08 +0200

    first commit

--- bookstore/store/urls.py
+++ bookstore/store/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.store_views),
+    path('', views.store_views, name='store'),
+    path('login/',views.login_view, name='login'),
+    path('signup/', views.signup_view, name='signup')
 ]
--- bookstore/store/views.py
+++ bookstore/store/views.py
@@ -1,5 +1,32 @@
-from django.shortcuts import render
+from django.contrib.auth import login 
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
 def store_views(request):
     return render(request, 'store/store.html')
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreateionForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    
+    return render(request, 'store/signup.html', {'form': form})
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            if 'next' in request.POST:
+                return redirect(request.POST.get('next'))
+            else:
+                return redirect('login')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'store/login.html', {'form': form})

0c9c6a3397a571e73e96873bac27fc5ddefbce51
leviwasa <example@gamil.com>
2025-01-19 00:16:14 +0200

    added users style and home path

--- bookstore/bookstore/urls.py
+++ bookstore/bookstore/urls.py
@@ -17,11 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from store import views as Home_store
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('store/', include('store.urls')),
     path('users/', include('users.urls')),
+    path('', Home_store.store_views, name='home'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()

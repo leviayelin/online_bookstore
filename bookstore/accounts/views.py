b85cc8bd59aecb3d2ea902bcf69a946c4e3a032b
leviwasa <example@gamil.com>
2025-01-27 23:46:53 +0200

    new changes to style

--- bookstore/accounts/views.py
+++ bookstore/accounts/views.py
@@ -1,18 +1,17 @@
-from django.contrib.auth import login 
+from django.contrib.auth import login, logout
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
 def signup_view(request):
     if request.method == 'POST':
-        form = UserCreateionForm(request.POST)
+        form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect('store:home')
     else:
         form = UserCreationForm()
-    
     return render(request, 'accounts/signup.html', {'form': form})
 
 def login_view(request):
@@ -28,3 +27,8 @@ def login_view(request):
     else:
         form = AuthenticationForm()
     return render(request, 'accounts/login.html', {'form': form})
+
+
+# def logout_view(request):
+#     if request.method == "POST"
+
